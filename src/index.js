4fe5e651688afbb1e2fd51e5ff59fb2d4d8e7e15
Stanislav Sheinoha <stanyslav.sheinoha@gmail.com>
2023-01-24 18:33:32 +0200

    Adds some code:
    - creates fetchCountries.js and writes code;
    - edits code style.css;
    - writes code index.js.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import './css/styles.css';
+// імпортуємо стилі
 import debounce from 'lodash.debounce';
+// імпортуємо бібліотеку lodash.debounce, спершу її завантаживши через npm
 import Notiflix from 'notiflix';
+// імпортуємо бібліотеку notiflix, спершу її завантаживши через npm
+import { fetchCountries } from './js/fetchCountries';
+// імпортуємо функцію fetchCountries, яка робить HTTP-запит на ресурс
+// і повертає проміс з масивом країн - результатом запиту
 
 const DEBOUNCE_DELAY = 300;
+//створюємо сталу змінну, яка слугуватиме позначенням часового інтервалу,
+// для прийому debounce, який у свою чергу після такої паузи між івентами
+// дасть змогу викликати функцію для обробки цього ж івенту
 const refs = {
   input: document.querySelector('#search-box'),
   countryList: document.querySelector('.country-list'),
   countryInfo: document.querySelector('.country-info'),
 };
+// створюємо змінні й ініціалізуємо їх адресами до відповідних елементів html
+
 // console.log();

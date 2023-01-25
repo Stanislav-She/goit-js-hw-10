998d52b502bac6c6131aca736617e715d21ef492
Stanislav Sheinoha <stanyslav.sheinoha@gmail.com>
2023-01-25 15:45:48 +0200

    Adds many files:
    - js code & new files;
    - css code;
    - html code.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,98 @@
 import './css/styles.css';
 // імпортуємо стилі
+import { fetchCountries } from './js/fetchCountries';
+// імпортуємо функцію fetchCountries, яка робить HTTP-запит на ресурс
+// і повертає проміс з масивом країн - результатом запиту
+// import { getCountryData } from './js/getCountryData';
+import { countryMarkup } from './js/countryMarkup';
+import { countryListMarkup } from './js/countryListMarkup';
+import { removeMarkup } from './js/removeMarkup';
 import debounce from 'lodash.debounce';
 // імпортуємо бібліотеку lodash.debounce, спершу її завантаживши через npm
 import Notiflix from 'notiflix';
 // імпортуємо бібліотеку notiflix, спершу її завантаживши через npm
-import { fetchCountries } from './js/fetchCountries';
-// імпортуємо функцію fetchCountries, яка робить HTTP-запит на ресурс
-// і повертає проміс з масивом країн - результатом запиту
 
 const DEBOUNCE_DELAY = 300;
 //створюємо сталу змінну, яка слугуватиме позначенням часового інтервалу,
 // для прийому debounce, який у свою чергу після такої паузи між івентами
 // дасть змогу викликати функцію для обробки цього ж івенту
+
 const refs = {
-  input: document.querySelector('#search-box'),
-  countryList: document.querySelector('.country-list'),
-  countryInfo: document.querySelector('.country-info'),
+  input: document.querySelector('#search__box'),
+  countryList: document.querySelector('.country__list'),
+  countryInfo: document.querySelector('.country__info'),
 };
 // створюємо змінні й ініціалізуємо їх адресами до відповідних елементів html
 
-// console.log();
+refs.input.addEventListener('input', debounce(getCountryData, DEBOUNCE_DELAY));
+// прикріплюємо слухача подій до інпута, також використовуємо метод debounce
+// з бібліотеки lodash.debounce аби викликалась колбек-функція, але після трисекундної
+// паузи
+
+function getCountryData(event) {
+  // створюємо функцію, яка зпрацьовуватиме під час події інпуту
+  const countryName = event.target.value.trim();
+  // створюємо константу значенням якої виступає значення введене користувачем до
+  // інпуту та в якому за допомогою методу .trim() санітизовано (видалено) на початку та кінці пробіли
+  if (!countryName) {
+    // якщо будь-яке нове значення назви країни вписується до інпута, то
+    // для ліста та інфо викликається колбек-функція, яка їх (поля) приймає та
+    // затерає з допомогою методу .innerHTML раніше внесений зміст
+    removeMarkup(refs.countryList);
+    removeMarkup(refs.countryInfo);
+    return;
+  }
+
+  fetchCountries(countryName)
+    // методом фетч ми робимо запит до API сервісу та отримуємо відповідь бекенду
+    // у вигляді масивів, які об'єднані в рядок методом join(). fetchCountries
+    // викликаємо з імпортів та передаємо туди значення введене до інпуту
+    .then(data => {
+      // метод .then повертає сформований promise
+      if (data.length > 10) {
+        //якщо результат збігів більше 10, то викликаються два колбека з параметрами
+        // countryList та countryInfo, чим видаляються вмісти їхні
+        removeMarkup(refs.countryList);
+        removeMarkup(refs.countryInfo);
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+          // додатково виводиться такий інфо-алерт через бібліотеку Notiflix
+        );
+        return data;
+      }
+      if (data.length >= 2 && data.length <= 10) {
+        // також тут же якщо результат збігів більше або дорівнює 2,
+        // то викликаються два колбека з параметрамиo сuntryList та countryInfo, чим видаляються вмісти їхні.
+        removeMarkup(refs.countryInfo);
+        removeMarkup(refs.countryList);
+        refs.countryList.insertAdjacentHTML(
+          // також до countryList записується інформація методом .insertAdjacentHTML()
+          // перед кінцем розмітки додається нова викликаючи колбек countryListMarkup,
+          // яка оперує отриманою data
+          'beforeend',
+          countryListMarkup(data)
+        );
+        countryListMarkup(data);
+        // повертає дані countryListMarkup(data)
+        return data;
+      } else {
+        removeMarkup(refs.countryInfo);
+        removeMarkup(refs.countryList);
+        // то викликаються два колбека з параметрамиo сuntryList та countryInfo, чим видаляються вмісти їхні.
+        refs.countryInfo.insertAdjacentHTML('beforeend', countryMarkup(data));
+        // також до countryInfo записується інформація методом .insertAdjacentHTML()
+        // перед кінцем розмітки додається нова викликаючи колбек countryMarkup,
+        // яка оперує отриманою data
+        countryMarkup(data);
+        // повертає дані countryMarkup(data)
+        return data;
+      }
+    })
+    .catch(err => {
+      removeMarkup(refs.countryList);
+      removeMarkup(refs.countryInfo);
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+    });
+  // якщо проміс взагалі не сформований, то знову ж таки зачищається вміст полів
+  // колбеками removeMarkup та через бібліотеку Notiflix виводиться сповіщення
+}
---
+++ src/js/countryMarkup.js
@@ -0,0 +1,29 @@
+export function countryMarkup(countryData) {
+  // колбек, яка викликається у функції getCountryData та приймає data масив
+  // із об'єктом даних про країну, яка повернута за запитом із API, у вигляді ключів
+  // та значень
+  return countryData
+    .map(({ flags, name, capital, population, languages }) => {
+      // Метод map() використовуємо для трансформації масиву, а саме перебираємо
+      // кожен об'єкт отриманий у масиві, який ми ДЕСТРУКТУРИЗУЄМО взявши в такі дужки {}.
+      return `
+  <div class="country__container">
+  <img class="country__flag" src="${flags.svg}" alt="flag" width="250px">
+  <p class="country__name">${name.official}</p>
+  </div>
+   <ul class="country__description">
+  <li class="country__item">&#127988 Capital:
+  <span class="country__span">${capital}</span>
+  </li>
+   <li class="country__item">&#128106 Population:
+  <span class="country__span">${population}</span>
+  </li>
+   <li class="country__item">&#128172 Languages:
+  <span class="country__span">${Object.values(languages).join(', ')}</span>
+  </li>
+  </ul>`;
+      // повертаємо такий рядок із розміткою, де підставляються значення з отриманих ключів
+    })
+    .join('');
+  // методом .join() об'єднуємо всі елементи отриманого масиву в рядок
+}
